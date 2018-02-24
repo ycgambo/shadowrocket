736c6a20adc834356cda4b730872bc06dcd9ca7e
gavin kwok <ycgambo@outlook.com>
2018-02-24 11:40:15 +0800

    add Autoloader.php, add ycgambo's email

--- ShadowRocket.php
+++ ShadowRocket.php
@@ -1,8 +1,10 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/src/shadowrocket/Autoloader.php';
 
 use ShadowRocket\Launcher;
 use ShadowRocket\Server;
 
+new \ShadowRocket\Autoloader();
 $sr = new Launcher();
 $sr->launch(array(new Server()));
---
+++ src/shadowrocket/Autoloader.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ShadowRocket;
+
+class Autoloader {
+    private $psr4 = array(
+        'ShadowRocket\\'    => 'src/shadowrocket'
+    );
+
+    private $project_root = '';
+
+    public function __construct() {
+        $this->project_root = dirname(dirname(dirname(__FILE__)));
+
+        spl_autoload_register(array($this, 'psr4Loader'));
+    }
+
+    public function psr4Loader($name) {
+        foreach ($this->psr4 as $namespace => $path) {
+            if (strpos($name, $namespace) !== FALSE) {
+                $path = strtr($path, '\\', DIRECTORY_SEPARATOR);
+                if (substr($path, -1, 1) !== DIRECTORY_SEPARATOR) {
+                    $path .= DIRECTORY_SEPARATOR;
+                }
+
+                $count = 1;
+                $file_path = str_ireplace($namespace, $path, $name, $count) . '.php';
+
+                if (file_exists($file_path)) {
+                    require_once $file_path;
+                    return TRUE;
+                }
+            }
+        }
+        return FALSE;
+    }
+}
