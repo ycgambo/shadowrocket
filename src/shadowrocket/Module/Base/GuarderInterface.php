1ba9bb4dcd6c21630dc0f47364cf4822c3f563a0
ycgambo <17816875364@163.com>
2018-05-01 21:40:18 +0800

    Update Guarder: inspect between remote and client

--- src/shadowrocket/Module/Base/GuarderInterface.php
+++ src/shadowrocket/Module/Base/GuarderInterface.php
@@ -4,7 +4,7 @@
  *
  * @file       GuarderInterface.php
  * @author     ycgambo
- * @update     4/30/18 8:20 AM
+ * @update     4/30/18 8:34 AM
  * @copyright  shadowrocket <https://github.com/ycgambo/shadowrocket>
  * @license    MIT License <http://www.opensource.org/licenses/mit-license.html>
  */
@@ -13,6 +13,28 @@ namespace ShadowRocket\Module\Base;
 
 interface GuarderInterface
 {
+    /**
+     * If false returned, service will be stopped, no more port listening.
+     *
+     * @param $request array
+     * @param $port
+     * @return mixed
+     */
     public function deny($request, $port);
+
+    /**
+     * If false returned, connection request will be refused.
+     *
+     * @param $request array
+     * @param $port
+     * @return mixed
+     */
     public function block($request, $port);
+
+    /**
+     * If false returned, connection between client and remote will be closed.
+     * @param $data string  the data come in and go out this server
+     * @return mixed
+     */
+    public function inspectFailed($data);
 }
--- src/shadowrocket/Module/Guarder.php
+++ src/shadowrocket/Module/Guarder.php
@@ -18,12 +18,6 @@ use ShadowRocket\Module\Base\LauncherModuleInterface;
 
 class Guarder extends ConfigRequired implements LauncherModuleInterface, GuarderInterface
 {
-    // todo: replace counter with database search
-    /**
-     * BE AWARE, each process has it's own counter, it's not share
-     */
-    static $counter = array();
-
     public function init()
     {
         $this->declareRequiredConfig(array(
@@ -40,48 +34,32 @@ class Guarder extends ConfigRequired implements LauncherModuleInterface, Guarder
         }
     }
 
-    /**
-     * This stops a service
-     * @param $request
-     * @param $port
-     * @return boolean
-     */
     public function _deny($request, $port)
     {
         return $this->getConfig('instance')->deny($request, $port);
     }
 
-    /**
-     * This blocks a request
-     * @param $request
-     * @param $port
-     * @return boolean
-     */
     public function _block($request, $port)
     {
         return $this->getConfig('instance')->block($request, $port);
     }
 
+    public function _inspectFailed($data)
+    {
+        return $this->getConfig('instance')->inspectFailed($data);
+    }
+
     public function deny($request, $port)
     {
         return false;
-
-        if (isset(self::$counter[$port])) {
-            self::$counter[$port] = self::$counter[$port] + 1;
-        } else {
-            self::$counter[$port] = 0;
-        }
-
-        if (self::$counter[$port] < 5) {
-            return true;
-        } else {
-            // return false will close worker on this port, we have to reset counter for new process
-            self::$counter[$port] = 0;
-            return false;
-        }
     }
 
     public function block($request, $prot)
+    {
+        return false;
+    }
+
+    public function inspectFailed($data)
     {
         return false;
     }
